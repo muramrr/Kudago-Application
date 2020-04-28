a35ba029ba137ab904a314fb8ffda72ccb28f55f
Andrii Kovalchuk <muram4ik@gmail.com>
2020-04-28 14:58:46 +0300

    added navigation

--- app/src/main/java/com/mmdev/kudago/app/MainActivity.kt
+++ app/src/main/java/com/mmdev/kudago/app/MainActivity.kt
@@ -25,5 +25,7 @@ class MainActivity: AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
 	}
+
 }
